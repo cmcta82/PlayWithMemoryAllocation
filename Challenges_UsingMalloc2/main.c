6eec3fbc0c974338f8ab0591c4d6fd6adff9f983
cmcta82 <cmcta82@gmail.com>
2021-03-11 09:11:09 +0100

    Initial Commit

---
+++ Challenges_UsingMalloc2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Challenges_UsingMalloc2
+//
+//  Created by Carlos Tavares on 11/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

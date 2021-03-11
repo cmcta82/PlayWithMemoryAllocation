5d9e075f31f296cdd037f9fa240daf88c16aad02
cmcta82 <cmcta82@gmail.com>
2021-03-11 11:08:14 +0100

    Finnish

--- Challenges_UsingMalloc2/main.c
+++ Challenges_UsingMalloc2/main.c
@@ -1,14 +1,58 @@
-//
-//  main.c
-//  Challenges_UsingMalloc2
-//
-//  Created by Carlos Tavares on 11/03/2021.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main(int argc, char *argv[]){
+    
+    char *stringMemory = NULL;
+    stringMemory = (char*)calloc(4, (sizeof(char)));
+    
+    //Use a copy of stringMemory to modify the address for each chars
+    char *stringMemoryTemp = stringMemory;
+    
+    char ch = '\0';
+    
+    //keep the size of stringMemory and increment 4bytes if needed (stringmemory + memoryByte = last address available)
+    int memoryByte = (4 *(sizeof(char)));
+
+    
+    printf("Please enter one Name: ");
+    
+    //get the name from the console
+    while((ch = getchar()) !='\n'){
+
+        /*keep a gap of 4bytes between the size of stringMemoryStart (malloc) end the total chars entererd by user
+        Make a realloc to add 2bytes to stringMemory */
+        if(stringMemoryTemp == (stringMemory + memoryByte)){
+            
+            //Realloc to get more 4bytes in memory
+            stringMemory = (char*)realloc(stringMemory, (memoryByte + 4));
+            
+            //getback to the last address to stringMemoryTemp
+            stringMemoryTemp = (stringMemory + memoryByte);
+            memoryByte += 4;
+
+        }
+        
+        //save the char on memory
+        *stringMemoryTemp = ch;
+        
+        //go to next address in memory
+        ++stringMemoryTemp;
+        
+    }
+    
+    //add Null char at the end of the string
+    *stringMemoryTemp = '\0';
+    
+    //Print the result
+    printf("\nThe name you entered is: %s\n", (stringMemory));
+    
+    /*Clean the memory*/
+    free(stringMemory);
+    stringMemory = NULL;
+    stringMemoryTemp = NULL;
+    
     return 0;
 }
